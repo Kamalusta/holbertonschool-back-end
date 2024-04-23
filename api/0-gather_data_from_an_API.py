e7cf7ccb35c4cbca213612ee59ea1f13eaa12bb6
Kamalusta <kamalusta@gmail.com>
2024-04-23 23:39:28 +0400

    correction

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """doc"""
 
-import urllib.request
 import requests
 import json
 import sys
 
 
-id = sys.argv[1]
-res1 = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
-response1 = json.loads(res1.text)
-nameusr = response1.get('name')
-res = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/todos')
-response = json.loads(res.text)
-done = 0
-ndone = 0
-# print(response)
-for res in response:
-    if res.get('completed'):
-        done += 1
-    else:
-        ndone += 1
+def main(id):
 
-print(f"Employee {nameusr} is done with tasks({done}/{ndone}):")
-for res in response:
-    if res.get('completed'):
-        print(f"\t {res.get('title')}")
+    res1 = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    response1 = json.loads(res1.text)
+    nameusr = response1.get('name')
+    res = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{id}/todos')
+    response = json.loads(res.text)
+    done = 0
+    ndone = 0
+
+    for res in response:
+        if res.get('completed'):
+            done += 1
+        else:
+            ndone += 1
+
+    print(f"Employee {nameusr} is done with tasks({done}/{ndone + done}):")
+    for res in response:
+        if res.get('completed'):
+            print(f"\t {res.get('title')}")
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    main(id)
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 """doc"""
 
-import requests
-import json
-import sys
+
 import csv
+import json
+import requests
+import sys
 
 
-id = sys.argv[1]
-res1 = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
-response1 = json.loads(res1.text)
-nameusr = response1.get('name')
-res = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/todos')
-response = json.loads(res.text)
+if __name__ == "__main__":
+    id = sys.argv[1]
+    res1 = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    response1 = json.loads(res1.text)
+    nameusr = response1.get('name')
+    res = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{id}/todos')
+    response = json.loads(res.text)
 
-data = []
-for todo in response:
-    values = [id, nameusr, todo.get("completed"), todo.get("title")]
-    data.append(values)
+    data = []
+    for todo in response:
+        values = [id, nameusr, todo.get("completed"), todo.get("title")]
+        data.append(values)
 
-filename = f'{id}.csv'
-with open(filename, 'w', newline="") as file:
-    csvwriter = csv.writer(file)
-    csvwriter.writerows(data)
+    filename = f'{id}.csv'
+    with open(filename, 'w', newline="") as file:
+        csvwriter = csv.writer(file)
+        csvwriter.writerows(data)
